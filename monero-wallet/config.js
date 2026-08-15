47383991fa762d03e4e4a2bf1c3b1ba08c302111
SeraTecnologia <seratecnologia@email.com>
2026-08-15 15:42:44 -0300

    auto: 2026-08-15 15:42

--- monero-wallet/config.js
+++
@@ -1,5 +0,0 @@
-const SECRET_ADMIN = (process.env.ADMIN_PATH || '/admin').replace(/\/+$/, '');
-const SITE_NAME = process.env.SITE_NAME || 'MoneroWallet';
-const SITE_URL = process.env.SITE_URL || 'http://localhost:3001';
-
-module.exports = { SECRET_ADMIN, SITE_NAME, SITE_URL };
